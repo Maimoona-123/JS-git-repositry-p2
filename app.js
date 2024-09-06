15887b0cb9067b6a3b205012d2c308d6996c3d23
maimoona <maimoonaaltaf7@gmail.com>
2024-09-06 05:16:09 -0700

    2nd Assignment

---
+++ app.js
@@ -0,0 +1,81 @@
+// Part-2   chap-1   ( Alerts );
+
+// Q-1
+// var firstName = "Maimoona";
+// alert ( firstName );
+
+// var lastName = "Mukhtiar Hussain";
+// alert ( lastName );
+
+// var email = "maimonaaltaf7@gmail.com";
+// alert ( email );
+
+// var number = "03167944505";
+// alert ( number );
+
+// var password = "maimoona123";
+// alert ( password );
+
+
+// Q-2
+
+
+
+// Q-3
+alert ( "Hi! Welcome my JavaScript Web page...." )
+
+
+
+// Chap-2   ( Numbers for String );
+
+
+// Q-1
+var camelCase = "firstName lastName iPad";
+console.log ( camelCase )
+
+
+// Q-2
+
+
+
+
+// Q-3
+// var teamName = "Brainwave Builders."
+// alert ( teamName )
+
+
+// Q-4
+var physics = "Newton";
+console.log ( physics );
+
+
+// Chap-3   ( Variables for numbers )
+
+
+// Q-1
+var msg1 = "“caseQty”";
+console.log ( msg1 );
+
+
+// Q-2
+var num = "144";
+console.log ( num );
+
+
+// Q-3
+var num = "9";
+console.log ( num );
+
+
+// Q-4
+var num =  2 + 2 ;
+console.log ( num ) ;
+
+
+// Q-5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+
+console.log ( merchTotal + shippingCharge );
+
